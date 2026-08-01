46139ae801f940ce34d4227018a24a986f4efa7a
Shubhayan Bhattacharjee <shubhb284@gmail.com>
2026-08-01 20:05:30 +0530

    Remove test files from repository

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: "node",
-  transform: {},
-  testMatch: [
-    "**/tests/unit/**/*.test.js",
-    "**/tests/integration/**/*.test.js",
-  ],
-  testTimeout: 20000, // integration tests spin up mongodb-memory-server, give them room
-};
